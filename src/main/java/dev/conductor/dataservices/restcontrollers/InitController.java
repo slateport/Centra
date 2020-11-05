6f593027482cb3a32a09cd5036cb27df6b4c752a
Shamil Nunhuck <shamil@shamil.co.uk>
2020-11-05 10:09:19 +0000

    Added WebApp with login feature

--- src/main/java/dev/conductor/dataservices/restcontrollers/InitController.java
+++ src/main/java/dev/conductor/dataservices/restcontrollers/InitController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 
 
 @RestController
-@RequestMapping("/init")
+@RequestMapping("/api/init")
 public class InitController {
 
     @Autowired
--- src/main/java/dev/conductor/dataservices/restcontrollers/WorkflowController.java
+++ src/main/java/dev/conductor/dataservices/restcontrollers/WorkflowController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/workflow")
+@RequestMapping("/api/workflow")
 public class WorkflowController {
 
     @Autowired
