3c2948cff07a2dcbb5faf0b5702e9e1afcad5b5f
Shamil Nunhuck <shamil@shamil.co.uk>
2020-11-07 17:57:10 +0000

    Extracted Issue view to IssueComponent

---
+++ src/main/webapp/src/pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+
+class SearchPage extends React.Component<any, any>{
+
+    render() {
+        return (
+            <React.Fragment>
+                <span/>
+            </React.Fragment>
+        )
+    }
+}
