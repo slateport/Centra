66fde3a6a2b49ce1b3198b7e5c326c0aa6928471
Shamil Nunhuck <shamil@shamil.co.uk>
2020-11-15 20:30:35 +0000

    Renders the assignee as a person in the audit history

--- src/main/webapp/src/pages/ViewIssuePage/components/AuditHistoryComponent.tsx
+++ src/main/webapp/src/pages/ViewIssuePage/components/AuditHistoryComponent.tsx
@@ -95,7 +95,6 @@ export default class AuditHistory extends Component<IAuditHistoryProps, any> {
     }
 
     render() {
-        console.log(blacklistedChanges)
         return (
             <React.Fragment>
                 <TableContainer>
