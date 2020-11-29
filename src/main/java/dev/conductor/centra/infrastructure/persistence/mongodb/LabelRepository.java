4ce397f141893dce81deabd9ad56ca1925cc8ec8
Shamil Nunhuck <shamil@shamil.co.uk>
2020-11-29 21:55:51 +0000

    Added Label Repository and port

---
+++ src/main/java/dev/conductor/centra/infrastructure/persistence/mongodb/LabelRepository.java
@@ -0,0 +1,6 @@
+package dev.conductor.centra.infrastructure.persistence.mongodb;
+
+import dev.conductor.centra.domain.label.spi.LabelPersistencePort;
+
+public interface LabelRepository extends LabelPersistencePort {
+}
