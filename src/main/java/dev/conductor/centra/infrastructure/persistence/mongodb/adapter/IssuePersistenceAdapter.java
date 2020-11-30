b93fd8cce2b10c29f997bea1f667519ac2743675
Shamil Nunhuck <shamil@shamil.co.uk>
2020-11-30 13:18:14 +0000

    Made IssuePersistencePort use IssueEntity

--- src/main/java/dev/conductor/centra/infrastructure/persistence/mongodb/adapter/IssuePersistenceAdapter.java
+++ src/main/java/dev/conductor/centra/infrastructure/persistence/mongodb/adapter/IssuePersistenceAdapter.java
@@ -2,45 +2,57 @@ package dev.conductor.centra.infrastructure.persistence.mongodb.adapter;
 
 import dev.conductor.centra.domain.issue.entity.Issue;
 import dev.conductor.centra.domain.issue.spi.IssuePersistencePort;
+import dev.conductor.centra.infrastructure.persistence.mongodb.entity.IssueEntity;
 import dev.conductor.centra.infrastructure.persistence.mongodb.repository.IssueRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class IssuePersistenceAdapter implements IssuePersistencePort {
 
     @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
     IssueRepository repository;
 
     @Override
     public List<Issue> findByProjectId(String projectId) {
-        return repository.findByProjectId(projectId);
+        List<Issue> issueList = new ArrayList<>();
+
+        for (IssueEntity issueEntity : repository.findByProjectId(projectId)) {
+            issueList.add(modelMapper.map(issueEntity, Issue.class));
+        }
+
+        return issueList;
     }
 
     @Override
     public Issue findByProjectIdAndExternalId(String projectId, long externalId) {
-        return repository.findByProjectIdAndExternalId(projectId, externalId);
+        return modelMapper.map(repository.findByProjectIdAndExternalId(projectId, externalId), Issue.class);
     }
 
     @Override
     public Issue findFirstByProjectIdOrderByLastModifiedDateDesc(String projectId) {
-        return repository.findFirstByProjectIdOrderByLastModifiedDateDesc(projectId);
+        return modelMapper.map(repository.findFirstByProjectIdOrderByLastModifiedDateDesc(projectId), Issue.class);
     }
 
     @Override
     public Issue findFirstByProjectIdOrderByCreatedDateDesc(String projectId) {
-        return repository.findFirstByProjectIdOrderByCreatedDateDesc(projectId);
+        return modelMapper.map(repository.findFirstByProjectIdOrderByCreatedDateDesc(projectId), Issue.class);
     }
 
     @Override
     public Issue save(Issue issue) {
-        return repository.save(issue);
+        return modelMapper.map(repository.save(modelMapper.map(issue, IssueEntity.class)), Issue.class);
     }
 
     @Override
     public void delete(Issue issue) {
-        repository.delete(issue);
+        repository.delete(modelMapper.map(issue, IssueEntity.class));
     }
 }
--- src/main/java/dev/conductor/centra/infrastructure/persistence/mongodb/repository/IssueRepository.java
+++ src/main/java/dev/conductor/centra/infrastructure/persistence/mongodb/repository/IssueRepository.java
@@ -1,16 +1,17 @@
 package dev.conductor.centra.infrastructure.persistence.mongodb.repository;
 
 import dev.conductor.centra.domain.issue.entity.Issue;
+import dev.conductor.centra.infrastructure.persistence.mongodb.entity.IssueEntity;
 import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
 @JaversSpringDataAuditable
-public interface IssueRepository extends MongoRepository<Issue, String> {
+public interface IssueRepository extends MongoRepository<IssueEntity, String> {
 
-    List<Issue> findByProjectId (String projectId);
-    Issue findByProjectIdAndExternalId(String projectId, long externalId);
-    Issue findFirstByProjectIdOrderByLastModifiedDateDesc(String projectId);
-    Issue findFirstByProjectIdOrderByCreatedDateDesc(String projectId);
+    List<IssueEntity> findByProjectId (String projectId);
+    IssueEntity findByProjectIdAndExternalId(String projectId, long externalId);
+    IssueEntity findFirstByProjectIdOrderByLastModifiedDateDesc(String projectId);
+    IssueEntity findFirstByProjectIdOrderByCreatedDateDesc(String projectId);
 }
