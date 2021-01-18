bbd6f063a69e46f7df4fd4e305059a842f58cc77
Shamil Nunhuck <shamil@shamil.co.uk>
2021-01-18 15:50:03 +0000

    Added User Association

---
+++ src/main/java/dev/conductor/centra/domain/issue/api/impl/IssueWatchServiceAdapter.java
@@ -0,0 +1,56 @@
+package dev.conductor.centra.domain.issue.api.impl;
+
+import dev.conductor.centra.domain.applicationUser.api.ApplicationUserService;
+import dev.conductor.centra.domain.applicationUser.entiity.ApplicationUser;
+import dev.conductor.centra.domain.issue.api.IssueWatchService;
+import dev.conductor.centra.domain.issue.entity.Issue;
+import dev.conductor.centra.domain.userAssociation.api.UserAssociationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IssueWatchServiceAdapter implements IssueWatchService {
+
+    private final UserAssociationService userAssociationService;
+    private final ApplicationUserService userService;
+
+    @Autowired
+    public IssueWatchServiceAdapter(UserAssociationService userAssociationService, ApplicationUserService userService) {
+        this.userAssociationService = userAssociationService;
+        this.userService = userService;
+    }
+
+    @Override
+    public void watchIssue(ApplicationUser user, Issue issue) {
+        userAssociationService.createAssociation(
+                IssueWatchService.ASSOCIATION_TYPE,
+                IssueWatchService.SINK_ENTITY_NAME,
+                user.getId(),
+                issue.getId()
+        );
+    }
+
+    @Override
+    public void unwatchIssue(ApplicationUser user, Issue issue) {
+        userAssociationService.findAssociationBySink(
+                IssueWatchService.ASSOCIATION_TYPE,
+                IssueWatchService.SINK_ENTITY_NAME,
+                issue.getId()
+        ).forEach(association -> {
+           if (association.getSourceId().equals(user.getId())){
+               userAssociationService.deleteAssociation(association);
+           }
+        });
+    }
+
+    @Override
+    public List<ApplicationUser> getApplicationUsersWatchingIssue(Issue issue) {
+        return userAssociationService.findAssociationBySink(
+                IssueWatchService.ASSOCIATION_TYPE, IssueWatchService.SINK_ENTITY_NAME, issue.getId()
+        )
+                .stream()
+                .map(association -> userService.findById(association.getSourceId()))
+                .collect(Collectors.toList());
+    }
+}
