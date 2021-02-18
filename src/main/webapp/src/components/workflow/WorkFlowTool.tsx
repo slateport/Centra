5605892220f18de639388471e53014038e452102
pa-solution2020 <p.a.developmentsolutions@gmail.com>
2021-02-19 03:49:10 +0800

    add bg and label function

--- src/main/webapp/src/components/workflow/WorkFlowTool.tsx
+++ src/main/webapp/src/components/workflow/WorkFlowTool.tsx
@@ -1,12 +1,15 @@
-import { Card, CardContent, Grid } from '@material-ui/core';
+import { Button, Card, CardContent, Grid, Input } from '@material-ui/core';
 import UndoIcon from '@material-ui/icons/Undo';
 import RedoIcon from '@material-ui/icons/Redo';
 import DeleteIcon from '@material-ui/icons/Delete';
-import {Button as MuiButton, Menu, MenuItem} from '@material-ui/core';
-import React, { useEffect, useState } from 'react'
+import GridOnIcon from '@material-ui/icons/GridOn';
+import {Button as MuiButton, Divider as MuiDivider, Typography as MuiTypography,} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { SketchPicker, ChromePicker } from 'react-color';
 import Draggable from 'react-draggable';
 import styled from 'styled-components';
 import CanvasEditTool from './CanvasEditTool';
+import {spacing, SpacingProps} from "@material-ui/system";
 
 const FlowEditor = styled.div`
     width: 100%;
@@ -23,9 +26,21 @@ const EditToolBar = styled.div`
     background: linear-gradient(rgba(230, 230, 230, 0.77) 0px, rgba(230, 230, 230 ,0.66), rgba(230, 230, 230, 0.89) 100%);
     border-bottom: 1px solid rgba(21, 29, 44, 0.1);
     padding: 5px;
+    display: flex;
 `;
 
-const Button = styled(MuiButton)`
+const ActionTool = styled.div`
+    width: 50%;
+    height: 100%;
+    text-align: left;
+`;
+
+const PropertyTool = styled.div`
+    width: 50%;
+    height: 100%;
+    text-align: right;
+`;
+const Button1 = styled(MuiButton)`
     color : ${props=> props.theme.header.color}
 `;
 
@@ -41,6 +56,10 @@ const BlackDeleteIcon = styled(DeleteIcon)`
     color: #000000;
 `;
 
+const BlackGridIcon = styled(GridOnIcon)`
+    color: #000000;
+`;
+
 const EditBoard = styled.div`
     width: 100%;
     display: flex;
@@ -48,7 +67,7 @@ const EditBoard = styled.div`
 `;
 
 const EditMenuBar = styled.div`
-    width: 200px;
+    width: 300px;
     height: 100%;
     background: rgba(198, 198, 198, 0.44);
     border-right: 1px solid rgba(21, 29, 44, 0.1);
@@ -58,6 +77,7 @@ const EditMenuBar = styled.div`
 const EditMenuBack = styled.div`
     width: 100%;
     height: 100%;
+    margin-top: 50px;
 `;
 
 const EditMenuContent = styled.div`
@@ -93,6 +113,18 @@ const CircleStarter = styled.div`
     margin: 10px auto 0 auto;
 `;
 
+const ColorPicker = styled(ChromePicker)`
+    margin: 20px auto 0 auto;
+`;
+
+interface TypographyPropsType extends SpacingProps {
+    component?: string
+}
+
+const Typography = styled(MuiTypography)<TypographyPropsType>(spacing);
+
+const Divider = styled(MuiDivider)(spacing);
+
 export default class WorkFlowTool extends React.Component<any, any>{
     private canvas: CanvasEditTool;
     constructor(props) {
@@ -100,6 +132,10 @@ export default class WorkFlowTool extends React.Component<any, any>{
         this.state = {
             squareModelState: false,
             circleStarterState: false,
+            color: '#c0c0c0',
+            selectedFigure: null,
+            grid: false,
+            figureLabel: null,
         }
     }
 
@@ -113,17 +149,18 @@ export default class WorkFlowTool extends React.Component<any, any>{
             if(data.x > 120 ) {
                 switch(data.node.id) {
                     case 'rectangle':
-                        this.createNode(data.node.id, data.x -140, data.y + 70);
+                        this.createNode(data.node.id, data.x -190, data.y + 110);
                         this.setState({squareModelState: false})
                         break;
                     case 'circle':
-                        this.createNode(data.node.id, data.x - 125, data.y + 10);
+                        this.createNode(data.node.id, data.x - 175, data.y + 50);
                         this.setState({circleStarterState: false})
                         break;
                 }
             }
         }
     }
+
 
     createNode(type, x, y){
         this.canvas.addNewNode(type, x, y);
@@ -135,8 +172,50 @@ export default class WorkFlowTool extends React.Component<any, any>{
         this.canvas.undo();
     }
 
+    redo() {
+        this.canvas.redo(); 
+    }
+
     delete() {
         this.canvas.delete();
+    }
+
+    showGrid() {
+        if(!this.state.grid) {
+            this.canvas.setGridView();
+            this.setState({grid: true});
+        } else {
+            this.canvas.disableGridView();
+            this.setState({grid: false});
+        }
+    }
+
+    handleMouseClick() {
+        // console.log('selected figure=====+>', this.canvas.getSelectedFigure().getBackgroundColor( ));
+        if(this.canvas.getSelectedFigure()) {
+            this.setState({selectedFigure: this.canvas.getSelectedFigure()});
+            let bgColor = this.canvas.getSelectedFigure().getBackgroundColor();
+            this.setState({color: {r: bgColor.red, g: bgColor.green, b: bgColor.blue, a: bgColor.alpha}});
+        } else {
+            this.setState({selectedFigure: null});
+        }
+        
+    }
+
+    handleChange = (color) => {
+        console.log('color =========>', color);
+        this.setState({ color: color.hex });
+        this.canvas.changeBGColor(this.state.selectedFigure, color.hex);
+    }
+
+    changeLabel = (e) => {
+        console.log(e.target.value);
+        this.setState({figureLabel: e.target.value});
+    }
+
+    addLabel = () => {
+        console.log('==========>?');
+        this.canvas.setFigureLabel(this.state.figureLabel)
     }
 
 
@@ -144,15 +223,22 @@ export default class WorkFlowTool extends React.Component<any, any>{
         return (
             <FlowEditor>
                 <EditToolBar>
-                    <Button onClick={() => this.undo()}>
-                        <BlackUndoIcon ></BlackUndoIcon>
-                    </Button>
-                    <Button>
-                        <BlackRedoIcon></BlackRedoIcon>
-                    </Button>
-                    <Button onClick={() => this.delete()}>
-                        <BlackDeleteIcon></BlackDeleteIcon>
-                    </Button>
+                    <ActionTool>
+                        <Button1 onClick={() => this.undo()}>
+                            <BlackUndoIcon ></BlackUndoIcon>
+                        </Button1>
+                        <Button1 onClick={() => this.redo()}>
+                            <BlackRedoIcon></BlackRedoIcon>
+                        </Button1>
+                        <Button1 onClick={() => this.delete()}>
+                            <BlackDeleteIcon></BlackDeleteIcon>
+                        </Button1>
+                    </ActionTool>
+                    <PropertyTool>
+                        <Button1 onClick={() => this.showGrid()}>
+                            <BlackGridIcon></BlackGridIcon>
+                        </Button1>
+                    </PropertyTool>
                 </EditToolBar>
                 <EditBoard>
                 <EditMenuBar>
@@ -161,6 +247,9 @@ export default class WorkFlowTool extends React.Component<any, any>{
                         <SquareNode></SquareNode>
                     </EditMenuBack>
                     <EditMenuContent>
+                        <Typography variant="h6" p={2} mt={1} gutterBottom >
+                            Step Shapes
+                        </Typography>
                         <Draggable
                             onDrag={(event, data) => this.modelHandleEvent(event, data)}
                             onStart={(event, data) => this.modelHandleEvent(event, data)}
@@ -177,10 +266,35 @@ export default class WorkFlowTool extends React.Component<any, any>{
                         >
                             <SquareNode className="drag-box" id="rectangle"></SquareNode>
                         </Draggable>
+                        {this.state.selectedFigure && (
+                            <>
+                                <Divider my={1} mt={5}/>
+                                <Typography variant="h6" p={2}  gutterBottom>
+                                    Add Label
+                                </Typography>
+                                <Input
+                                    id="label"
+                                    name="label"
+                                    autoComplete="label"
+                                    autoFocus
+                                    style={{marginLeft: 5, marginRight: 15}}
+                                    value={this.state.figureLabel}
+                                    onChange={this.changeLabel}
+                                />
+                                <Button color="primary" onClick={() => this.addLabel()}>Add</Button>
+                                <Typography variant="h6" p={2}  gutterBottom>
+                                    Background Color
+                                </Typography>
+                                <ColorPicker 
+                                    color={this.state.color}
+                                    onChange={ this.handleChange }>
+                                </ColorPicker>
+                            </>
+                        )}
                     </EditMenuContent>
                 </EditMenuBar>
                 <CanvasView>
-                    <div id="workflowtool" style={{width: '100%', height: '100%'}}></div>
+                    <div id="workflowtool" style={{width: '100%', height: '100%'}} onClick={() => this.handleMouseClick()}></div>
                 </CanvasView>
                 </EditBoard>
             </FlowEditor>
